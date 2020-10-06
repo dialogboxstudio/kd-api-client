a5a66bab7ac07594b5fc48a47676f86489262d76
Victor <victor.dialogbox@gmail.com>
2020-10-06 05:46:57 +0300

    Create OrderStatus.php

---
+++ src/Entity/OrderStatus.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace DialogBoxStudio\KdClient\Entity;
+
+
+use Psr\Http\Message\ResponseInterface;
+
+class OrderStatus
+{
+    private object $response;
+
+    public function __construct(ResponseInterface $response)
+    {
+        $this->response = $response;
+    }
+
+    public function getResult(): string
+    {
+        return $this->response->getBody()->getContents();
+    }
+
+    public function getHttpStatus(): int
+    {
+        return $this->response->getStatusCode();
+    }
+
+    public function getStatus(): string
+    {
+        $array = json_decode($this->getResult(), true);
+        return $array['result']['status'];
+    }
+
+}
