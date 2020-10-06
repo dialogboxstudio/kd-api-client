fcf1afee087b5bba04bf4ade17d9d70cf6a01879
Victor <victor.dialogbox@gmail.com>
2020-10-06 05:46:53 +0300

    Create Marketing.php

---
+++ src/Entity/Marketing.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace DialogBoxStudio\KdClient\Entity;
+
+
+use Psr\Http\Message\ResponseInterface;
+
+class Marketing
+{
+    private object $response;
+
+    public function __construct(ResponseInterface $response)
+    {
+        $this->response = $response;
+    }
+
+    public function getResult(): string
+    {
+        return $this->response->getBody()->getContents();
+    }
+
+    public function getHttpStatus(): int
+    {
+        return $this->response->getStatusCode();
+    }
+
+    public function getStatus(): string
+    {
+        $array = json_decode($this->getResult(), true);
+        return $array['result']['status'];
+    }
+
+    public function getLink(): string
+    {
+        $array = json_decode($this->getResult(), true);
+        return $array['result']['link'];
+    }
+
+    public function getMarketing(): array
+    {
+        $array = json_decode($this->getResult(), true);
+        return $array['result']['marketing'];
+    }
+
+
+}
