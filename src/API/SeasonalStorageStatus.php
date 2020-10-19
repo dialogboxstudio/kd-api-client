6e4ba5f1304bdb4a30e8b2d26d403eda23d80e25
Victor <victor.dialogbox@gmail.com>
2020-10-19 21:46:51 +0300

    Update SeasonalStorageStatus.php

--- src/API/SeasonalStorageStatus.php
+++ src/API/SeasonalStorageStatus.php
@@ -6,5 +6,48 @@ namespace DialogBoxStudio\KdClient\API;
 
 class SeasonalStorageStatus
 {
+    private string $result;
+
+    private bool $is_error;
+
+    private object $data;
+
+    private Error $error;
+
+    public function __construct(string $result, bool $is_error = false)
+    {
+        $this->result = $result;
+        $this->is_error = $is_error;
+        if ($is_error === true) {
+            $this->error = new Error($result);
+        } else {
+            $this->data = json_decode($this->result);
+        }
+    }
+
+    public function getResult(): string
+    {
+        return $this->result;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->data->result->status;
+    }
+
+    public function isError(): bool
+    {
+        return $this->is_error;
+    }
+
+    public function getError(): Error
+    {
+        return $this->error;
+    }
+
+    public function getSeasonalStorageStatus(): string
+    {
+        return $this->data->result->seasonalStorageStatus;
+    }
 
 }
